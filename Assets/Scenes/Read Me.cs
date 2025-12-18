52ca633f5c3911def84cf59295a0666aa81df61d
Kai13131 <haiyang.he13@gmail.com>
2025-12-17 20:11:09 -0500

    final

---
+++ Assets/Scenes/Read Me.cs
@@ -0,0 +1,12 @@
+/*
+ This is a read me file for the project.
+
+    This is a classic Snake game made with Unity.
+    As a snake, you navigate around the screen, eating food to grow longer 
+        while avoiding collisions with yourself.
+
+    Controls: Use the arrow keys to change the snake's direction.
+    goal: Eat as much food as possible without running into yourself.
+
+    Enjoy the game!
+ */
